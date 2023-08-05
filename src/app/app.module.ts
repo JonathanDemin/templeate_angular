e54bd9b32b445ec32fdad98da614f32742878c7f
JonathanDemin <jonydemin@gmail.com>
2023-08-05 15:57:59 -0300

    asdad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ServicioComponent } from './servicio/servicio.component';
+import { BoxComponent } from './servicio/box/box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ServicioComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    HttpClienteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
